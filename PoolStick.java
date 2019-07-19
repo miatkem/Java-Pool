80e62d1a791404aed63f0b6fab70a5b49ec6323e
Madison Miatke <miatkem@gmail.com>
2019-07-19 11:15:34 -0400

    Add files via upload

--- PoolStick.java
+++ PoolStick.java
@@ -8,67 +8,67 @@ import java.awt.event.*;
 
 public class PoolStick implements MouseListener, MouseMotionListener 
 {
- double xPower, yPower;
- double xMouse, yMouse;
- double xCue, yCue;
- boolean onScreen, release;
- 
- public void paint(Graphics g)
- {
-  if(onScreen)
-  {
-   Graphics2D g2 = (Graphics2D) g;
-   g2.setColor(Color.BLACK);
-   g2.draw(new Line2D.Double(xMouse, yMouse, xCue, yCue));
-  }
- }
- public void tick(Table table) 
- {
-  xCue=table.getBalls()[0].getX();
-  yCue=table.getBalls()[0].getY();
-  if(release)
-  {
-   
-   table.getBalls()[0].shoot(xPower,yPower);
-   release=false;
-  }
- }
- 
- @Override
- public void mouseClicked(MouseEvent e) {
-  release=true;
- }
-
- @Override
- public void mouseEntered(MouseEvent e) {
-  onScreen=true;
- }
-
- @Override
- public void mouseExited(MouseEvent e) {
-  onScreen=false;
- }
-
- @Override
- public void mousePressed(MouseEvent e) {
- }
-
- @Override
- public void mouseReleased(MouseEvent e) {
-  release=true;
- }
-
- @Override
- public void mouseDragged(MouseEvent e) {
- }
-
- @Override
- public void mouseMoved(MouseEvent e) {
-  xMouse=e.getX();
-  yMouse=e.getY();
-  xPower=(xCue-xMouse)/5;
-  yPower=(yCue-yMouse)/5;
- }
- 
- 
+    double xPower, yPower;
+    double xMouse, yMouse;
+    double xCue, yCue;
+    boolean onScreen, release;
+    int shots;
+    
+    public void paint(Graphics g)
+    {
+        if(onScreen)
+        {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(Color.BLACK);
+            g2.draw(new Line2D.Double(xMouse, yMouse, xCue, yCue));
+        }
+    }
+    public void tick(Table table) 
+    {
+        //Track mouse, when released give cue ball speed proportionate to its distance from the mouse
+        xCue=table.getBalls()[0].getX();
+        yCue=table.getBalls()[0].getY();
+        if(release)
+        {
+            shots++;
+            table.getBalls()[0].shoot(xPower,yPower);
+            release=false;
+        }
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        release=true;
+    }
+    
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        onScreen=true;
+    }
+    
+    @Override
+    public void mouseExited(MouseEvent e) {
+        onScreen=false;
+    }
+    
+    @Override
+    public void mousePressed(MouseEvent e) {}
+    
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        release=true;
+    }
+    
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+    
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        xMouse=e.getX();
+        yMouse=e.getY();
+        xPower=(xCue-xMouse)/5;
+        yPower=(yCue-yMouse)/5;
+    }
+    
+    
 }
