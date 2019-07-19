7f4dd4334690ca7fc5e43677a9d5fe0475174210
Madison Miatke <miatkem@gmail.com>
2019-07-19 11:15:28 -0400

    Add files via upload

--- PoolRunner.java
+++ PoolRunner.java
@@ -1,34 +1,26 @@
-
-
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 
 public class PoolRunner{
- PoolGame game;
- public static void main(String[] args) {
-  new PoolRunner();
- }
- 
- public PoolRunner()
- {
-  game = new PoolGame();
-  JFrame frame = new JFrame();
- 
-  frame.setResizable(false);
-  frame.setSize(288,402);
-  frame.setVisible(true);
-  frame.setLocationRelativeTo(null);
-  frame.add(game.getGraphics());
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  game.start();
- }
-
+    PoolGame game;
+    //MAIN-starts game immediately
+    public static void main(String[] args) {
+        new PoolRunner();
+    }
+    
+    public PoolRunner()
+    {
+        game = new PoolGame();
+        
+        //set up main frame
+        JFrame frame = new JFrame();
+        frame.setResizable(false);
+        frame.setSize(392,402);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.add(game.getWindow());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        game.start();
+    }
+    
 }
