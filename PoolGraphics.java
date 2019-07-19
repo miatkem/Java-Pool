39c4fe57f623bc39f9399014b9091da59e407424
Madison Miatke <miatkem@gmail.com>
2019-07-19 11:15:21 -0400

    Add files via upload

--- PoolGraphics.java
+++ PoolGraphics.java
@@ -2,32 +2,34 @@ import javax.swing.JPanel;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 
 public class PoolGraphics extends JPanel{
- Table pooltable;
- 
- public PoolGraphics(Table tab)
- {
-  pooltable=tab;
-  this.setSize(282, 373);
-  this.setBackground(Color.WHITE);
-  this.setFocusable(false); 
-  addMouseMotionListener(pooltable.getStick());
-  addMouseListener(pooltable.getStick());
- }
- 
- @Override
- public void paintComponent(Graphics g)
- { 
-  super.paintComponent(g);
-  BufferedImage buffer = new BufferedImage(282, 373, 3);
-  pooltable.paint(buffer.getGraphics()); 
-  g.drawImage(buffer, 0,0,this);
- }
- 
- public void updateGraphics()
- {
-  repaint();
- }
+    Table pooltable;
+    
+    public PoolGraphics(Table tab)
+    {
+        pooltable=tab;
+        this.setSize(282, 373);
+        this.setBackground(Color.WHITE);
+        this.setFocusable(false); 
+        addMouseMotionListener(pooltable.getStick());
+        addMouseListener(pooltable.getStick());
+    }
+    
+    @Override
+    public void paintComponent(Graphics g)
+    { 
+        super.paintComponent(g);
+        //Use buffered image as a buffer to draw
+        BufferedImage buffer = new BufferedImage(282, 373, 3);
+        pooltable.paint(buffer.getGraphics());  //pooltable paints to buffer
+        g.drawImage(buffer, 0,0,this); //paint buffer to screen
+    }
+    
+    public void updateGraphics()
+    {
+        repaint();
+    }
 }
