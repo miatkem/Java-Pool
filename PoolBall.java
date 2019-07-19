b587b61ed912b33883aaa59437041cbfe17b6ddf
Madison Miatke <miatkem@gmail.com>
2019-07-19 11:15:02 -0400

    Add files via upload

--- PoolBall.java
+++ PoolBall.java
@@ -8,44 +8,32 @@ public class PoolBall {
  double xPosition, yPosition;
  double radius;
  int number;
- boolean isCueBall,collided;
  boolean inPocket;
  
  public PoolBall(double xPosition, double yPosition, int number)
  {
-  this.xPosition=xPosition;
-  this.yPosition=yPosition;
-  ySpeed=xSpeed=0;
-  this.number=number; //pool ball number
-  radius=10.0;
-  if(number==0) //0 means cue ball
-   isCueBall=true;
-  else 
-   isCueBall=false;
-  inPocket=false;
+     this.xPosition=xPosition;
+     this.yPosition=yPosition;
+     ySpeed=xSpeed=0;
+     this.number=number; //pool ball number
+     radius=10.0;
+     inPocket=false;
  }
  
  public PoolBall(double xPosition, double yPosition, int number, double radius)
  {
-  
-  
-  this.xPosition=xPosition;
-  this.yPosition=yPosition;
-  this.number=number;
-  this.radius=radius;
-  if(number==0)
-  {
-   ySpeed=0;
-   isCueBall=true;
-  }
-  else 
-   isCueBall=false;
-  inPocket=false;
+     this.xPosition=xPosition;
+     this.yPosition=yPosition;
+     this.number=number;
+     this.radius=radius;
+     ySpeed=xSpeed=0;
+     inPocket=false;
  }
  
  public void tick(Table poolTable)
  {
      if(!inPocket){
+         //Set Slow Speed to 0
          if(Math.abs(xSpeed)<.15)
              xSpeed=0.0;
          if(Math.abs(ySpeed)<.15)
@@ -54,12 +42,12 @@ public class PoolBall {
          xPosition+=xSpeed;
          yPosition+=ySpeed;
          
+         //Bounce off Walls
          if(xPosition-radius<=Table.LEFTWALL)
          {
              xPosition=Table.LEFTWALL+radius;
              xSpeed*=-1;
          }
-         
          if(xPosition+radius>=Table.RIGHTWALL)
          {
              xPosition=Table.RIGHTWALL-radius;
@@ -70,89 +58,69 @@ public class PoolBall {
              yPosition=Table.TOPWALL+radius;
              ySpeed*=-1;
          }
-         
          if(yPosition+radius>=Table.BOTTOMWALL)
          {
              yPosition=Table.BOTTOMWALL-radius;
              ySpeed*=-1;
          }
+         
+         //Friction Force
          xSpeed=xSpeed*poolTable.getFriction();
          ySpeed=ySpeed*poolTable.getFriction();
-         
-         if(Math.abs(xSpeed)<.15)
-             xSpeed=0.0;
-         if(Math.abs(ySpeed)<.15)
-             ySpeed=0.0;
      }
- }
- 
- public int getNumber() 
- {
-  return number;
- }
-
- public boolean isCueBall()
- {
-  return isCueBall();
- }
- 
- public double getX()
- {
-  return xPosition;
- }
- 
- public double getY()
- {
-  return yPosition;
- }
- 
- public double getRadius()
- {
-  return radius;
  }
  
  public void paint(Graphics g)
  {
-  Graphics2D g2 = (Graphics2D) g;
-  switch(number)
-  {
-   case 0: g2.setColor(Color.WHITE); break;
-   case 1: 
-   case 9: g2.setColor(Color.YELLOW); break;
-   case 2: 
-   case 10: g2.setColor(Color.BLUE); break;
-   case 3: 
-   case 11: g2.setColor(Color.RED); break;
-   case 4: 
-   case 12: g2.setColor(new Color(128,0,128)); break;
-   case 5: 
-   case 13: g2.setColor(new Color(255,140,0)); break;
-   case 6: 
-   case 14: g2.setColor(Color.GREEN); break;
-   case 7: 
-   case 15: g2.setColor(new Color(128,0,0)); break;
-   case 8: g2.setColor(Color.BLACK); break;
-  }
-  g2.fill(new Ellipse2D.Double(xPosition-radius,yPosition-radius,radius*2,radius*2));
-  g2.setColor(Color.BLACK);
-  g2.draw(new Ellipse2D.Double(xPosition-radius,yPosition-radius,radius*2,radius*2));
-  
+     Graphics2D g2 = (Graphics2D) g;
+     //Fill Circle
+     switch(number)
+     {
+         case 0: g2.setColor(Color.WHITE); break;
+         case 1: 
+         case 9: g2.setColor(Color.YELLOW); break;
+         case 2: 
+         case 10: g2.setColor(Color.BLUE); break;
+         case 3: 
+         case 11: g2.setColor(Color.RED); break;
+         case 4: 
+         case 12: g2.setColor(new Color(128,0,128)); break;
+         case 5: 
+         case 13: g2.setColor(new Color(255,140,0)); break;
+         case 6: 
+         case 14: g2.setColor(Color.GREEN); break;
+         case 7: 
+         case 15: g2.setColor(new Color(128,0,0)); break;
+         case 8: g2.setColor(Color.BLACK); break;
+     }
+     g2.fill(new Ellipse2D.Double(xPosition-radius,yPosition-radius,radius*2,radius*2));
+     //Draw Outline
+     g2.setColor(Color.BLACK);
+     g2.draw(new Ellipse2D.Double(xPosition-radius,yPosition-radius,radius*2,radius*2));
  }
-
+ 
  public boolean isMoving() {
-  if(Math.abs(xSpeed)>0||Math.abs(ySpeed)>0)
-   return true;
-  return false;
+     if(Math.abs(xSpeed)>0||Math.abs(ySpeed)>0)
+         return true;
+     return false;
  }
-
+ 
  public void shoot(double xPow, double yPow) {
-  xSpeed+=xPow;
-  ySpeed+=yPow;
+     xSpeed+=xPow;
+     ySpeed+=yPow;
  }
  
  public void setVel(double xVel,double yVel)
  {
-  xSpeed=xVel;
-  ySpeed=yVel;
+     xSpeed=xVel;
+     ySpeed=yVel;
  }
+ 
+ public int getNumber() { return number; }
+ 
+ public double getX() { return xPosition; }
+ 
+ public double getY() { return yPosition; }
+ 
+ public double getRadius() { return radius; }
 }
